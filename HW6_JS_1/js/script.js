3df7c9dfdfcf70d1295ce631ca097bc83e4c2772
ISVru <isv_ru@mail.ru>
2021-10-22 12:30:56 +0300

    +HW6_JS_1

--- HW6_JS_1/js/script.js
+++ HW6_JS_1/js/script.js
@@ -2,11 +2,37 @@
 
 console.log("[Задание 1] Задайте высоту, длину и ширину прямоугольного параллелепипеда и найти его площадь." +
     "Решение : " +
-    "Задаем параметры (Высота a=7),(Длина b=9),(Ширина c=5)" +
-    "Формула площади пп 2 * (a*b + b*c + a*c)");
-let a = 7; // Высота
-let b = 9; // Длина
-let c = 5; // Ширина
-let s = 2 * (a*b + b*c + a*c); // Площадь пп
-console.log("S = " + s);
+    "Задаем параметры (Высота =7),(Длина =9),(Ширина =5)" +
+    "Формула площади  S п.п. = 2(ab + ac + bc)");
+let height = 7; // Высота a
+let length = 9; // Длина b
+let width = 5; // Ширина c
+let square = 2 * (height*length + length*width + height*width); // Площадь пп
+console.log("площадь = " + square);
+
+console.log("[Задание 2] В переменной n хранится двузначное число. Найти сумму цифр числа n и вывести результат в консоль.");
+
+const Num = 77;
+const firstNum = Num % 10;
+const secondNum = (Num - firstNum) / 10;
+const sum = firstNum + secondNum;
+
+console.log("сумма = " + sum);
+
+console.log("[Задание 3] Поменять значение двух переменных, не используя третью (использовать арифметические операторы)" +
+    "let a = 7; let b = 3; // код console.log(a, b); // 3 7");
+
+let firstNumb = 7;
+let secondNumb = 3;
+let total = secondNumb + firstNumb;
+firstNumb = total - firstNumb;
+secondNumb = total - firstNumb;
+
+console.log("меняем значение " + firstNumb,secondNumb);
+
+
+
+
+
+
 
