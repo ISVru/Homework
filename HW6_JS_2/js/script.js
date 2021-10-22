25d03c23c25acf326b0404a81125af669c9e6240
ISVru <isv_ru@mail.ru>
2021-10-23 01:49:14 +0300

    HW6_JS_2_3final

--- HW6_JS_2/js/script.js
+++ HW6_JS_2/js/script.js
@@ -6,13 +6,53 @@ console.log("[Задание 1] Дана целочисленная переме
 let userNum = parseInt(prompt("Введите число"));
 if ( userNum <= 100 && userNum >= 90) {
 console.log("отлично");
-} else if ( userNum <= 89 && userNum >= 60); {
+} else if ( userNum <= 89 && userNum >= 60) {
 console.log("хорошо");
-} else if ( userNum <= 59 && userNum >= 40);{
+} else if ( userNum <= 59 && userNum >= 40) {
     console.log("удовлетворительно");
-} else if ( userNum <= 39 && userNum >= 0); {
+} else if ( userNum <= 39 && userNum >= 0) {
     console.log("попробуйте еще раз");
 } else{
-    console.log("")
+    console.log("");
 
 }
+
+console.log("[Задание 2] Задача на switch.В зависимости от введенного оператора нужно вывести в " +
+    "консоль результат сложения,вычитания, умножения или деления чисел, которые ввел пользователь");
+
+let firstOper = parseInt(prompt("Введите первый операнд")),
+    secondOper = parseInt(prompt("Введите второй операнд")),
+    operator = prompt("Введите оператор");
+switch (operator) {
+    case "+":
+        console.log("сложение " + firstOper + secondOper);
+        break;
+    case "-":
+        console.log("вычитание " + firstOper - secondOper);
+        break;
+    case "*":
+        console.log("умножение" + firstOper * secondOper);
+        break;
+    case "/":
+        console.log("деление" + firstOper / secondOper);
+        break;
+    default:
+        console.log("Некорректные данные");
+}
+
+console.log("[Задание 3] Задача на цикл while." +
+"Получить от пользователя количество тарелок и количество моющего средства");
+
+let cap = parseInt(prompt("Введите количество тарелок")),
+    washLiq = parseInt(prompt("Введите количество моющего средства"));
+while (cap > 0 && washLiq > 0) {
+    cap -= 1;
+    washLiq -= .5;
+    if (cap === 0 && washLiq >0) {
+        console.log("Остаток моющего средства  ${washLiq}.");
+    } else if (washLiq === 0 &&  cap > 0) {
+        console.log("Остаток грязных тарелок ${cap}.");
+    }
+    }
+
+console.log("[Задание 4] Программа загадывает число в диапазоне [1;9] (задать через Math.random)");
