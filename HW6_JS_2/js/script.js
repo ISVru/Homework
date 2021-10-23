a53588ab8e37ba693720859896e410034b790eb4
ISVru <isv_ru@mail.ru>
2021-10-24 01:37:12 +0300

    complit_4task

--- HW6_JS_2/js/script.js
+++ HW6_JS_2/js/script.js
@@ -1,18 +1,18 @@
 "use strict";
 
 console.log("[Задание 1] Дана целочисленная переменная count" +
-"количество верных ответов.В зависимости от значения этой переменной вывести в консоль оценку");
+    "количество верных ответов.В зависимости от значения этой переменной вывести в консоль оценку");
 
 let userNum = parseInt(prompt("Введите число"));
-if ( userNum <= 100 && userNum >= 90) {
-console.log("отлично");
-} else if ( userNum <= 89 && userNum >= 60) {
-console.log("хорошо");
-} else if ( userNum <= 59 && userNum >= 40) {
+if (userNum <= 100 && userNum >= 90) {
+    console.log("отлично");
+} else if (userNum <= 89 && userNum >= 60) {
+    console.log("хорошо");
+} else if (userNum <= 59 && userNum >= 40) {
     console.log("удовлетворительно");
-} else if ( userNum <= 39 && userNum >= 0) {
+} else if (userNum <= 39 && userNum >= 0) {
     console.log("попробуйте еще раз");
-} else{
+} else {
     console.log("");
 
 }
@@ -25,10 +25,10 @@ let firstOper = parseInt(prompt("Введите первый операнд")),
     operator = prompt("Введите оператор");
 switch (operator) {
     case "+":
-        console.log("сложение " + firstOper + secondOper);
+        console.log("сложение " + (firstOper + secondOper));
         break;
     case "-":
-        console.log("вычитание " + firstOper - secondOper);
+        console.log("вычитание " + (firstOper - secondOper));
         break;
     case "*":
         console.log("умножение" + firstOper * secondOper);
@@ -41,18 +41,39 @@ switch (operator) {
 }
 
 console.log("[Задание 3] Задача на цикл while." +
-"Получить от пользователя количество тарелок и количество моющего средства");
+    "Получить от пользователя количество тарелок и количество моющего средства");
 
 let cap = parseInt(prompt("Введите количество тарелок")),
     washLiq = parseInt(prompt("Введите количество моющего средства"));
 while (cap > 0 && washLiq > 0) {
     cap -= 1;
     washLiq -= .5;
-    if (cap === 0 && washLiq >0) {
-        console.log("Остаток моющего средства  {washLiq}");
-    } else if (washLiq === 0 &&  cap > 0) {
-        console.log("Остаток грязных тарелок {cap}");
-    }
-    }
+    console.log("Остаток средства" + washLiq);
+}
+if (cap === 0) {
+    console.log("Все тарелки вымыты. Остаток моющего средства" + washLiq);
+} else {
+    console.log("Моющее средство закончилось. Остаток не вымытых тарелок" + cap);
+}
+
 
 console.log("[Задание 4] Программа загадывает число в диапазоне [1;9] (задать через Math.random)");
+
+let userNumb, programNumb = Math.floor(Math.random() * 9) + 1;
+while (userNumb = parseInt(prompt("Введите число от 1 до 9"))) {
+    if (userNumb === 0) {
+        console.log("выход из программы")
+
+        break;
+    } else if (userNumb > programNumb && userNumb <= 9) {
+        console.log("загаданное число меньше");
+    } else if (userNumb < programNumb && userNumb <= 9) {
+        console.log("загаданное число больше");
+    } else if (userNumb === programNumb) {
+        console.log("Вы угадали число !");
+        break;
+
+    } else
+        console.log("Введено неверное число.Введите число от 1 до 9");
+
+}
