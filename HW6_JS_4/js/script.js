f040988213eb3e390af3f3794b394303071b0649
ISVru <isv_ru@mail.ru>
2021-11-05 19:56:02 +0300

    fix_HW6_JS_4+HW6_JS_5

--- HW6_JS_4/js/script.js
+++ HW6_JS_4/js/script.js
@@ -1,9 +1,9 @@
-"use strict";
+ "use strict";
 
 console.log("Задание 1");
 
 function productName(count) {
-    if (count % 10 === 1 && count != 11) return `${count} товар`;
+    if (count % 10 === 1 && count !== 11) return `${count} товар`;
     if ((count % 10 === 2 && count % 100 !== 12) ||
         (count % 10 === 3 && count % 100 !== 13) ||
         (count % 10 === 4 && count % 100 !== 14)) return `${count} товара`;
