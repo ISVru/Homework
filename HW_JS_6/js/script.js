bcdb25f0e832d66309c912ac69fd053273a871d6
ISVru <isv_ru@mail.ru>
2021-11-08 13:51:13 +0300

    +HW_JS_6

---
+++ HW_JS_6/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+console.log("Задание 1");
+
+let num = prompt('Введите число от 1 до 7');
+let result = num
+switch (result) {
+    case '1':
+        alert('Понедельник');
+        break;
+    case '2':
+        alert('Вторник');
+        break;
+    case '3':
+        alert('Среда');
+        break;
+    case '4':
+        alert('Четверг');
+        break;
+    case '5':
+        alert('Пятница');
+        break;
+    case '6':
+        alert('Суббота');
+        break;
+    case '7':
+        alert('Воскресенье');
+        break;
+}
+
+
+console.log("Задание 2");
