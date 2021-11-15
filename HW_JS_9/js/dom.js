5c704e9df6258304e5e72f5e290b7537ded1a81a
ISVru <isv_ru@mail.ru>
2021-11-15 15:02:32 +0300

    +HW_JS_9_beta

---
+++ HW_JS_9/js/dom.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let animals = [{
+        "name": "Люся",
+        "age": "1 год",
+        "color": "трехцветная",
+        "additional_info": { "vaccinations": true, "passport": true }
+    },
+    {
+        "name": "Том",
+        "age": "3 месяца",
+        "color": "белый",
+        "additional_info": { "vaccinations": false, "passport": false }
+    },
+    {
+        "name": "Макс",
+        "age": 2,
+        "color": "серый",
+        "additional_info": { "vaccinations": false, "passport": true }
+    },
+    {
+        "name": "Василий",
+        "age": 3,
+        "color": "черный",
+        "additional_info": { "vaccinations": true, "passport": true }
+    }
+];
